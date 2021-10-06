6f2319b7327cf42b0b05ecf74d412a6782c4cb2f
blakewebb15 <89742731+blakewebb15@users.noreply.github.com>
2021-10-06 19:54:34 -0400

    Refresh Button
    Forgot to push my refresh button

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -28,21 +28,26 @@ const Offer = {
         }
 
     },
+    methods: {
+        fetchUserData() {
+            fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then((parsedJson) => {
+                console.log(parsedJson);
+                this.person = parsedJson.results[0]
+                console.log("C");
+            })
+            .catch( err => {
+                console.error(err)
+            })
+
+            console.log("B");
+        }
+
+    },
     created() {
-        console.log("A");
-
-        fetch('https://randomuser.me/api/')
-        .then(response => response.json())
-        .then((parsedJson) => {
-            console.log(parsedJson);
-            this.person = parsedJson.results[0]
-            console.log("C");
-        })
-        .catch( err => {
-            console.error(err)
-        })
-
-        console.log("B");
+        return this.fetchUserData();
+     
     }
   }
   
